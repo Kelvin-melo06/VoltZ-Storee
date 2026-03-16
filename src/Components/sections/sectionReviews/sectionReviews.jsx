5d35c4fcd3665c5177a5cd3ec6bb0dd78f5d11dd
Kelvin Melo <kmelo7882@gmail.com>
2026-03-16 13:48:49 -0300

    feat(reviews): implementa seção de avaliações com grid responsivo e cards animados

--- src/Components/sections/sectionReviews/sectionReviews.jsx
+++ src/Components/sections/sectionReviews/sectionReviews.jsx
@@ -3,18 +3,62 @@ import Card from "../../ui/Card/card";
 
 const SectionReviews = () => {
   return (
-    <section className="reviews-section fade up" id="reviews">
-      <h2 className="section-title fade-up">O que Dizem nossos clientes:</h2>
-      <div className="reviews-grid">
-        <Card className="reviews-card fade-up">
-        <p>"Adorei o suporte premium, muito prático e resistente."</p>
-        <span>- Ana, Gamer</span>
+    <section
+      id="reviews"
+      className="
+    py-20
+    px-[5%]
+    bg-[#0D0D0D]
+    fade-up
+    "
+    >
+      <h2
+        className="
+        font-['Orbitron']
+        text-3xl
+        md:text-4xl
+        text-center
+        mb-12
+        text-[#F2F2F2]
+        fade-up
+      "
+      >
+        O que Dizem nossos clientes:
+      </h2>
+      <div
+        className="
+      grid
+      gap-10
+      mt-12
+      [grid-template-columns:repeat(auto-fit,minmax(300px,1fr))]
+      "
+      >
+        <Card
+          className="
+        reviews-card
+        "
+        >
+          <p>
+            <span className="absolute -top-0 -left-2 text-[#00BFFF] text-lg">
+              💬
+            </span>
+            "Adorei o suporte premium, muito prático e resistente."
+          </p>
+          <span>- Ana, Gamer</span>
         </Card>
-        <Card className="reviews-card fade-up">
-        <p>"Adorei o suporte premium, muito prático e resistente."</p>
-        <span>- Carlos, Gamer</span>
-
-        </Card >
+        <Card
+          className="
+        reviews-card fade-up
+        "
+        >
+          <p className="relative pl-5 italic">
+            <span className="absolute -top-1 -left-2 text-[#00BFFF] text-lg">
+              💬
+            </span>
+            "Adorei o suporte premium, muito prático e resistente."
+          </p>
+          <span>- Carlos, Gamer</span>
+        </Card>
       </div>
     </section>
   );
