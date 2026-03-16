1f35892690b19023acb94dc5bfeec7301e7a40e4
Kelvin Melo <kmelo7882@gmail.com>
2026-03-16 17:39:21 -0300

    fix bug de deploy

--- src/Components/sections/sectionOffers/sectionOffers.jsx
+++ src/Components/sections/sectionOffers/sectionOffers.jsx
@@ -1,5 +1,6 @@
 import "./sectionOffers.css";
 import Card from "../../ui/Card/card.jsx";
+import Button from '../../ui/button/Button.jsx'
 import SectionTitle from "../../ui/SectionTitle.jsx";
 
 const SectionOffers = () => {
