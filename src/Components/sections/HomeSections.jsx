3424bdf3d144f271a31957a41e47de5ac41608c8
Kelvin Melo <kmelo7882@gmail.com>
2026-03-19 17:10:13 -0300

    feat(doc HomeSection): Documentação HomeSection Feita

--- src/Components/sections/HomeSections.jsx
+++ src/Components/sections/HomeSections.jsx
@@ -1,3 +1,13 @@
+/**
+ * HomeSections.jsx
+ * ----------------------------------------
+ * Componente responsável por reunir todas as seções
+ * da página inicial da aplicação.
+ *
+ * Ele funciona como o "container principal" do conteúdo,
+ * organizando a estrutura visual do site.
+ */
+
 import SectionHero from "@/Components/sections/sectionHero/sectionHero";
 import SectionProducts from "@/Components/sections/sectionProducts/sectionProducts";
 import SectionReviews from "@/Components/sections/sectionReviews/sectionReviews";
@@ -8,18 +18,70 @@ import SectionFaq from "@/Components/sections/sectionFaq/sectionFaq";
 
 const HomeSections = () => {
   return (
-    <main className='pt-[80px]'>
-      
-        <SectionHero />
-        <SectionProducts />
-        <SectionReviews />
-        <SectionOffers />
-        <SectionNewsletter />
-        <SectionAbout />
-        <SectionFaq />
+    <main className="pt-[80px]">
+
+      {/**
+       * main:
+       * -----
+       * Tag semântica que representa o conteúdo principal da página.
+       */}
+
+      {/**
+       * padding-top:
+       * ------------
+       * Adicionado para compensar o Header fixo.
+       *
+       * Como o Header utiliza position: fixed,
+       * o conteúdo poderia ficar escondido atrás dele.
+       *
+       * Esse espaçamento garante que as seções comecem
+       * visíveis corretamente na tela.
+       */}
+
+      {/**
+       * Seções da aplicação:
+       * --------------------
+       * Cada componente representa uma parte do site:
+       *
+       * - SectionHero        -> seção inicial (destaque)
+       * - SectionProducts    -> produtos em destaque
+       * - SectionReviews     -> depoimentos de clientes
+       * - SectionOffers      -> ofertas/promos
+       * - SectionNewsletter  -> captura de e-mail
+       * - SectionAbout       -> informações sobre a marca
+       * - SectionFaq         -> perguntas frequentes
+       *
+       * Cada uma será documentada separadamente
+       * em seus respectivos arquivos.
+       */}
+
+      <SectionHero />
+      <SectionProducts />
+      <SectionReviews />
+      <SectionOffers />
+      <SectionNewsletter />
+      <SectionAbout />
+      <SectionFaq />
+
+      {/**
+       * Alias (@):
+       * ----------
+       * Utilizado para evitar caminhos relativos longos.
+       *
+       * Exemplo:
+       * "../../../Components/..."
+       *
+       * Com alias:
+       * "@/Components/..."
+       *
+       * Isso melhora:
+       * - Legibilidade
+       * - Manutenção
+       * - Organização do projeto
+       */}
       
     </main>
-  )
-}
+  );
+};
 
-export default HomeSections
+export default HomeSections;
